535b87e8f43178c9ad09f0203268c0a8cee3f7f8
mkruneva <marta.kruneva@gmail.com>
2017-08-29 11:49:56 +0100

    cube map, material

--- js/threejsFingerboard.js
+++ js/threejsFingerboard.js
@@ -2,7 +2,7 @@ if (!Detector.webgl) Detector.addGetWebGLMessage();
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set( -4.5, -47, 491 );
+camera.position.set(-4.5, -47, 491);
 
 //GUI 
 var gui = new dat.GUI();
@@ -14,39 +14,62 @@ renderer = createRenderer(0x222222);
 var parent = document.getElementById('canvasContainer');
 parent.appendChild(renderer.domElement);
 
+//Cubemap 
+
+var envMap = new THREE.CubeTextureLoader()
+    .setPath('tex/cubemap/')
+    .load([
+        'posx.jpg',
+        'negx.jpg',
+        'posy.jpg',
+        'negy.jpg',
+        'posz.jpg',
+        'negz.jpg',
+    ]);
+
+//scene.background = envMap;
+
 //TEX AND MAT
-var diffTex = new THREE.TextureLoader().load( "tex/beech_wood_albedo.jpg" );
+var diffTex = new THREE.TextureLoader().load("tex/beech_wood_albedo.jpg");
 diffTex.wrapS = THREE.RepeatWrapping;
 diffTex.wrapT = THREE.RepeatWrapping;
-diffTex.repeat.set( 3, 3 );
-var aoTex = new THREE.TextureLoader().load( "tex/beech_wood_ao.png" );
+diffTex.repeat.set(3, 3);
+var aoTex = new THREE.TextureLoader().load("tex/beech_wood_ao.png");
+var nrmTex = new THREE.TextureLoader().load("tex/beech_wood_anormal.png");
+nrmTex.wrapS = THREE.RepeatWrapping;
+nrmTex.wrapT = THREE.RepeatWrapping;
+nrmTex.repeat.set(3, 3);
+var roughtTex = new THREE.TextureLoader().load("tex/beech_wood_bump.jpg");
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var material2 = new THREE.MeshStandardMaterial({ aoMap: aoTex, color: 0xbbbbbb, map: diffTex, roughness: 0.7 });
+var material2 = new THREE.MeshStandardMaterial({ aoMap: aoTex, 
+                                                color: 0xbbbbbb, 
+                                                envMap: envMap, 
+                                                map: diffTex, 
+                                                normalMap: nrmTex, 
+                                                normalScale: new THREE.Vector3( 0.3, 0.3 ), 
+                                                roughness: 1,
+                                                roughnessMap: roughtTex
+                 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 
-//Plane
-// var geometry = new THREE.PlaneGeometry( 200, 200, 200 );
-// var plane = new THREE.Mesh( geometry, material );
-// scene.add( plane );
-
 //LIGHTS
-var ambLight = new THREE.AmbientLight( 0x404040 ); // soft white light
+var ambLight = new THREE.AmbientLight(0x404040); // soft white light
 ambLight.intensity = 5;
-scene.add( ambLight );
+scene.add(ambLight);
 
-var dirLight = new THREE.DirectionalLight( 0xFFFFFF );
-dirLight.rotation.set( 1, 0, 0 );
-dirLight.position.set( -200, 300, -100 );
-var helper = new THREE.DirectionalLightHelper( dirLight, 0.5 );
-scene.add( dirLight );
-scene.add( helper );
+// var dirLight = new THREE.DirectionalLight( 0xFFFFFF );
+// dirLight.rotation.set( 1, 0, 0 );
+// dirLight.position.set( -200, 300, -100 );
+// var helper = new THREE.DirectionalLightHelper( dirLight, 0.5 );
+// scene.add( dirLight );
+// scene.add( helper );
 
-var hemLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-scene.add( hemLight );
+var hemLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+scene.add(hemLight);
 
 // load a resource
 var loader = new THREE.OBJLoader();
@@ -60,8 +83,8 @@ loader.load(
                 child.material = material2;
             }
         });
-        object.rotation.set( 0, 0, 0 );
-        object.position.set( 0, -100, 0 );
+        object.rotation.set(0, 0, 0);
+        object.position.set(0, -100, 0);
         scene.add(object);
     }
 );
